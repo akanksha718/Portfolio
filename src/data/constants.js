58eaba3412ef382e472a2416017f4ca5aa2a9596
Akanksha negi <ankiakanksha707@gmail.com>
2025-12-21 20:21:37 +0530

    hi

--- src/data/constants.js
+++ src/data/constants.js
@@ -16,6 +16,7 @@ import quick from "../assets/quickgpt.png";
 import n8n from "../assets/n8n.png";
 import mnist from "../assets/mnist.png";
 import movie from "../assets/movie-rec.png";
+import focus from "../assets/focus.png";
 export const Bio = {
   name: "Akanksha Negi",
   roles: [
@@ -371,6 +372,17 @@ export const projects = [
     category: "web app",
     github: "https://github.com/akanksha718/Chatgpt-clone",
   },
+    {
+    id: 5,
+    title: "FocusLab",
+    date: "Jun 2021",
+    description:
+      " A Todo Web App made with React JS, Expressjs. It has a login page where users can log in with their Google account. It has a sidebar where users can see all the tasks and can create a new task",
+    image: focus,
+    tags: ["React Js", "Local Storage", "Firebase Auth", "Node JS"],
+    category: "web app",
+    github: "https://github.com/akanksha718/FocusLab",
+  },
   {
     id: 0,
     title: "Ecommerse Website",
@@ -427,20 +439,7 @@ export const projects = [
     category: "machine learning",
     github: "https://github.com/akanksha718/Movie-Recommendatoin-System",
     webapp: "https://whatsapp-clone-rishav.web.app",
-  },
-  // {
-  //   id: 5,
-  //   title: "Todo Web App",
-  //   date: "Jun 2021",
-  //   description:
-  //     " A Todo Web App made with React JS, Redux, and Material UI. It has a login page where users can log in with their Google account. It has a sidebar where users can see all the tasks and can create a new task. It has a calendar where users can see all the tasks on a particular date. It has a search bar where users can search for a particular task.",
-  //   image:
-  //     "https://camo.githubusercontent.com/84ac6ab6f378348ef28d8184062b7e9e3511a1252ae3966eaa49e8e998f732a7/68747470733a2f2f666972656261736573746f726167652e676f6f676c65617069732e636f6d2f76302f622f746f646f2d6170702d63386331392e61707073706f742e636f6d2f6f2f53637265656e73686f74253230283938292e706e673f616c743d6d6564696126746f6b656e3d33643335646366322d626666322d343730382d393031632d343232383866383332386633",
-  //   tags: ["React Js", "Local Storage", "AWS Auth", "Node JS"],
-  //   category: "web app",
-  //   github: "https://github.com/rishavchanda/Todo-Web-App",
-  //   webapp: "https://rishav-react-todo.netlify.app/",
-  // },
+  }
   // {
   //   id: 6,
   //   title: "Breaking Bad",
